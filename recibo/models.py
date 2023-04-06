d715f4007f1f05f588df32976dcc9ad15f7d10f9
MigueEC <filia.castillo@gmail.com>
2023-04-05 20:02:00 -0500

    DataBase

--- recibo/models.py
+++ recibo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class cliente(models.Model):
+    codigo = models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField(max_length=254, unique=True)
+    
+    def __str__(self):
+        texto = "{0}({1})"
+        return texto.format(self.nombre, self.codigo)
--- recibo_solucion/settings.py
+++ recibo_solucion/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'recibo_solucion.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'base_de_datos',
     }
 }
 
